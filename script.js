eec1eb77ddb27a469709a9d94a4997bc9df387c5
Isabela <isabeladuartelbg@gmail.com>
2019-06-12 16:48:24 -0300

    Exemplos de funçoes

---
+++ script.js
@@ -0,0 +1,54 @@
+// Construir uma escada
+// O material da escada pode ser * # $
+// Quantos desgraus ele quer
+
+// let material = prompt("Digite o material * # $: ");
+// let quantMaterial = material;
+// let degraus = Number (prompt ("Digite quantos degraus vai ter sua escada: "));
+
+// for (let i=0;i<degraus;i++){
+//     console.log(material);
+//     material = material + quantMaterial;
+// }
+
+
+// let idadeUser = Number(prompt("Digite quantos anos você tem: "));
+
+// let filmes = [
+//     {titulo:"Harry Potter", idade: 10},
+//     {titulo:"IT, a coisa",idade: 16},
+//     {titulo:"O albergue",idade: 18}
+// ]
+// for(i in filmes){
+//     console.log(filmes[i].titulo);
+
+//     if (idadeUser>=10){
+//         console.log(filmes[i]); 
+//     }
+//     else{
+//         alert ("Não vai assistir o filme");
+//     }
+// }
+
+// Bem vindos a função
+
+// function somar (x,y){
+//     console.log(x+y);
+// }
+// somar(10,4);
+
+
+// let valor1=Number(prompt("Digite o primeiro valor: "));
+// let valor2=Number(prompt("Digite o primeiro valor: "));
+
+// function somar(valor1,valor2){
+//     console.log (`Soma = ${valor1+valor2}`);
+//     console.log (`Subtração = ${valor1-valor2}`);
+//     console.log(`Divisão = ${valor1/valor2}`);
+//     console.log (`Multiplicação = ${valor1*valor2}`);
+// }
+// somar (valor1,valor2);
+
+// calculadora = (a,b) =>{
+//     console.log(a+b)
+// }
